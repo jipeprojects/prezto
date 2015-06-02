1018288104592d40e2786822f921545cdad8174b
Benjamin Chrétien <chretien@lirmm.fr>
2015-06-02 11:15:52 +0900

    Add setopt MONITOR (enable C-Z etc.)

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -29,6 +29,7 @@ unsetopt MAIL_WARNING     # Don't print a warning message if a mail file has bee
 setopt LONG_LIST_JOBS     # List jobs in the long format by default.
 setopt AUTO_RESUME        # Attempt to resume existing job before creating a new process.
 setopt NOTIFY             # Report status of background jobs immediately.
+setopt MONITOR            # Associate a job with each pipeline.
 unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
 unsetopt HUP              # Don't kill jobs on shell exit.
 unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
